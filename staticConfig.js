40dee2c3d61bbc37d0d1384e7edafabcf9e62cd3
bfernando <bruno.fernando@cloudwatt.com>
2016-04-13 16:02:20 +0200

    Add staticConfig.js : List of contrail ports

---
+++ staticConfig.js
@@ -0,0 +1,14 @@
+var staticConfig = {};
+staticConfig.port = {};
+
+staticConfig.port['contrail-api'] = staticConfig.port['ApiServer'] = 8082;
+staticConfig.port['contrail-svc-monitor'] = staticConfig.port['Service Monitor'] = 8088;
+staticConfig.port['contrail-schema'] = staticConfig.port['Schema'] = 8087;
+staticConfig.port['contrail-discovery'] = staticConfig.port['DiscoveryService'] = 5998;
+staticConfig.port['contrail-ifmap'] = staticConfig.port['IfmapServer'] = 8443;
+staticConfig.port['contrail-control'] = staticConfig.port['ControlNode'] = 8083;
+staticConfig.port['xmpp-server'] = 5269;
+staticConfig.port['contrail-vrouter'] = 8085;
+staticConfig.port['contrail-analytics-api'] = 8081;
+
+module.exports = staticConfig;
