b86b78d168ef8966189a06d6bb7dbb932a6d65a5
bfernando <bruno.fernando@cloudwatt.com>
2016-04-13 18:00:30 +0200

    Code refactoring for ContrailSet.js which contains the main data structure: * replace the control.js * more simple to read * add getJSON: fetch data from Disco, Analytics, Introspec ... * add updateSet: update each subset * add checkServices: check network port status for each subset * work only for Config and Control

--- ContrailSet.js
+++ ContrailSet.js
@@ -1,37 +1,123 @@
 var async = require('async');
-var unirest = require('unirest');
-var contrailNode = require('./contrailNode');
-var Service = require('./Service');
+var ConfigSet = require('./ConfigSet');
+var ControlSet = require('./ControlSet');
+//var VRouterSet = require('./VRouterSet');
+var ContrailNode = require('./ContrailNode');
+var DiscoveryClient = require('./DiscoveryClient');
+var util = require('util');
 
-var _requestJSON = function(href, callback){
-  unirest.get(href)
-  .header('application/json')
-  .end(function(response){
-    objJSON = response.body;
-    callback(null, objJSON);
-  });
+var ContrailSet = function(discovery, eventEmitter){
+  this.eventEmitter = eventEmitter;
+  this.discoveryClient = new DiscoveryClient(discovery);
+  this.configSet = new ConfigSet();
+  this.controlSet = new ControlSet();
+  //this.vRouterSet = new VRouterSet();
+  this.nodes={};
 };
 
-var _updateContrailSet = function(callback){
-  async.parallel([
+//@async
+ContrailSet.prototype.update = function(callback){
+  var self = this;
+  async.waterfall([
     function(callback){
-      contrailSet.configSet.updateFromDisco(callback);
+      self.getJSON(callback);
     },
     function(callback){
-      contrailSet.controlSet.update(callback);
+      self.updateSet(callback);
     },
     function(callback){
-      contrailSet.vRouterSet.update(callback);
+      self.checkServices(callback);
     }
-  ], function(err, res){
-    //console.log(contrailSet.configSet.nodes[0].services);
-    contrailNode.updateEvent.emit('updateEvent');
-    if(callback){
-      //console.log("");
-      callback(null);
-    }
+  ], function(err){
+    //console.log(util.inspect(self, { showHidden: true, depth: null, colors: false }));
+    // console.log(JSON.stringify(self.configSet));
+    // console.log(JSON.stringify(self.controlSet));
+    // process.exit(0);
+    //self.eventEmitter.emit('updated', self);
+    callback(null);
   });
 }
 
-exports.requestJSON = _requestJSON;
-exports.updateContrailSet = _updateContrailSet;
+//@async
+ContrailSet.prototype.getJSON = function(callback){
+  var self = this;
+  async.parallel([
+    function(callback){
+      self.discoveryClient.get(callback);
+    }
+    //self.analyticsClient.get
+  ], function(err){
+    //console.log(""+self.discoveryClient);
+    callback(null);
+  });
+}
+
+//@async
+ContrailSet.prototype.updateSet = function(callback){
+  var self = this;
+  async.parallel([
+    function(callback){
+      self.configSet.update(self.discoveryClient.path['/clients.json'].data, self.discoveryClient.path['/services.json'].data, callback);
+    },
+    function(callback){
+      self.controlSet.update(self.discoveryClient.path['/clients.json'].data, self.discoveryClient.path['/services.json'].data, callback);
+    }
+  ], function(err){
+    callback(null);
+  });
+}
+
+/*
+function(callback){
+contrailSet.vRouterSet.update(callback);
+}*/
+
+//@async
+ContrailSet.prototype.checkServices = function(callback){
+  var self = this;
+  async.parallel([
+    function(callback){
+      self.configSet.checkServices(callback);
+    },
+    function(callback){
+      self.controlSet.checkServices(callback);
+    }
+    /*
+    function(callback){
+      self.vRouterSet.check();
+    }*/
+  ], function(err){
+    callback(null);
+  });
+}
+
+ContrailSet.prototype.toString = function(){
+  return '###############"\n# ContrailSet #\n###############\n'+util.inspect(contrailSet, false, null, true);
+}
+/*
+var main = function(){
+  var contrailSet = new ContrailSet('localhost');
+
+  async.waterfall([
+    function(callback){
+      var self = contrailSet;
+      async.parallel([
+        function(callback){
+          console.log(util.inspect(this, null, 2));
+          callback(null);
+        },
+        function(callback){
+          self.discoveryClient.get(callback);
+        }
+      ], function(err){
+        callback(null);
+      });
+    }
+  ], function(err){
+    console.log(""+contrailSet.discoveryClient);
+  });
+}
+
+main();
+*/
+module.exports = ContrailSet;
