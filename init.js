b977b11b781c939d3c355aa10166eade95b20656
bfernando <bruno.fernando@cloudwatt.com>
2016-04-13 18:27:47 +0200

    Remove unused instruction in init.js

--- init.js
+++ init.js
@@ -1,40 +1,12 @@
 var async = require('async');
 var util = require('util');
 var unirest = require('unirest');
-var contrailNode = require('./contrailNode');
-var ConfigSet = require('./ConfigSet');
-var ControlSet = require('./ControlSet');
-var VRouterSet = require('./VRouterSet');
-var control = require('./control');
 
 global.config = {
   discovery : null,
   analytics : null,
   time : 4000
 };
-
-var initStructure = function(callback){
-  global.contrailSet = {
-    dataSource : global.config.discovery,
-    configSet : [],
-    controlSet : [],
-    vRouterSet : [],
-    nodes:{}
-  };
-  contrailSet.configSet = new ConfigSet('localhost');
-  contrailSet.controlSet = new ControlSet(CONFIG.analytics);
-  contrailSet.vRouterSet = new VRouterSet(CONFIG.analytics);
-  callback(null);
-}
-
-var _run = function(callback){
-  async.waterfall([
-    initStructure,
-    control.updateContrailSet
-  ], function(err, res){
-    callback(null);
-  });
-}
 
 var initFromEnv = function(){
   global.config.discovery = process.env.CONTRAIL_DISCOVERY_URL ||
@@ -56,7 +28,6 @@ var initFromOptions = function(program, callback){
   }
 }
 
-exports.run = _run;
 exports.initFromOptions = initFromOptions;
 exports.initFromEnv = initFromEnv;
 //global.CONFIG = CONFIG;
