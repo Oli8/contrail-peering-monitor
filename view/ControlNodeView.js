db7c510e522e658760a99273a12be234e3a1d170
bfernando <bruno.fernando@cloudwatt.com>
2016-04-18 14:44:30 +0200

    Add the view part for the Control nodes

---
+++ view/ControlNodeView.js
@@ -0,0 +1,67 @@
+var blessed = require('blessed');
+var contrib = require('blessed-contrib');
+var util = require('util');
+var utils = require('../src/utils');
+
+var ControlNodeView = function(data, width, offset){
+  this.data = data;
+  this.type = 'ControlNodeView';
+  this.view = initView(data.name, width, offset);
+}
+
+var initView = function(name, width, offset){
+  var table = contrib.table({
+    keys: true,
+    fg: 'white',
+    selectedFg: 'white',
+    selectedBg: 'blue',
+    interactive: true,
+    label: name,
+    left: offset+'%',
+    width: width+'%',
+    height: '90%',
+    border: {type: "line", fg: "cyan"},
+    columnSpacing: 5, //in chars
+    columnWidth: [20, 12] /*in chars*/
+  });
+  table.rows.style.selected.bg = undefined;
+  return table;
+}
+
+var parseData = function(data){
+  var result = {
+    headers: ['Service', 'Status'],
+    data:[]
+  };
+  for(i in data.services){
+    var service = data.services[i];
+    result.data.push([service['name'], service['status']]);
+  }
+  for(i in data.ifmapPeer.peer){
+    result.data.push(['IFMAP '+data.ifmapPeer.peer[i].status, data.ifmapPeer.peer[i].host]);
+  }
+  return result;
+}
+
+ControlNodeView.prototype.append = function(screen){
+  screen.append(this.view);
+}
+
+ControlNodeView.prototype.update = function(screen){
+  var dataSet = parseData(this.data);
+  this.view.setData(dataSet);
+}
+
+var main = function(screen){
+  utils.stdin(function(err, data){
+    //console.log(data[0]);
+    var result = parseData(data);
+    console.log('################\n# Parse Object #\n################\n'+require('util').inspect(result, { depth: null }));
+  });
+}
+
+if (require.main === module) {
+  main();
+}
+
+module.exports = ControlNodeView;
---
+++ view/ControlSetView.js
@@ -0,0 +1,54 @@
+var blessed = require('blessed');
+var contrib = require('blessed-contrib');
+var util = require('util');
+var utils = require('../src/utils');
+var ControlNodeView = require('./ControlNodeView');
+
+var ControlSetView = function(data, height, offset){
+  this.data = data;
+  this.type = 'ControlSetView';
+  this.view = initView(height, offset);
+  this.children = [];
+}
+
+var initView = function(height, offset){
+  var box = blessed.box({
+    top: offset+'%',
+    width: '50%',
+    left: '25%',
+    height: height+'%',
+    border: {
+      type: 'line'
+    },
+    style: {
+      fg: 'white',
+      bg: 'magenta',
+      border: {
+        fg: '#f0f0f0'
+      }
+    }
+  });
+  return box;
+}
+
+var parseData = function(data){
+
+}
+
+ControlSetView.prototype.append = function(screen){
+  screen.append(this.view);
+}
+
+ControlSetView.prototype.update = function(screen){
+  var nodes = this.data.nodes;
+  var offset = (100/nodes.length);
+
+  for(i in nodes){
+    this.children.push(new ControlNodeView(nodes[i], offset, i*offset));
+    //screen.log(nodes[i]);
+    this.view.append(this.children[i].view);
+    this.children[i].update(screen);
+  }
+}
+
+module.exports = ControlSetView;
